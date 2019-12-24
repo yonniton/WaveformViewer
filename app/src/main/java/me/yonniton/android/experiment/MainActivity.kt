9913a65022c897afc9aca22f5905d1711c36bbdd
Jonathan Teo <jtmh@hotmail.com>
2019-12-24 10:05:57 +0800

    🎉 project creation with Android Studio template app

---
+++ app/src/main/java/me/yonniton/android/experiment/MainActivity.kt
@@ -0,0 +1,19 @@
+package me.yonniton.android.experiment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.yonniton.android.experiment.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/me/yonniton/android/experiment/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package me.yonniton.android.experiment.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
