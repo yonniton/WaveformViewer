c22e570d9762cd431e3a0e853b37faca94d7e523
Jonathan Teo <Jonathan.Teo@dyson.com>
2021-03-25 23:10:19 +0800

    🚧 prototype alert-audio last 10s fade-out

--- app/src/main/java/me/yonniton/waveform/noisealert/MediaProvider.kt
+++ app/src/main/java/me/yonniton/waveform/noisealert/MediaProvider.kt
@@ -1,16 +1,22 @@
 package me.yonniton.waveform.noisealert
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.net.Uri
 import android.os.Environment
 import android.widget.Toast
+import androidx.core.animation.doOnEnd
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.Player.EventListener
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import me.yonniton.waveform.R
 import me.yonniton.waveform.common.AppSettings
+import java.util.concurrent.TimeUnit
+import kotlin.math.max
+import kotlin.math.min
 
 class MediaProvider(
     private val context: Context,
@@ -61,12 +67,32 @@ class MediaProvider(
             ?.takeUnless {
                 it.isPlaying
             } // ongoing playback uninterrupted
-            ?.run {
+            ?.apply {
                 Util.getUserAgent(context, context.getString(R.string.app_name))
                     .let { userAgentString -> DefaultDataSourceFactory(context, userAgentString) }
                     .let { dataSourceFactory -> ProgressiveMediaSource.Factory(dataSourceFactory) }
                     .let { progressiveMediaSourceFactory -> progressiveMediaSourceFactory.createMediaSource(uriAudio) }
                     .also { mediaSource -> prepare(mediaSource) }
+
+                val fader = ObjectAnimator.ofFloat(audioComponent, "volume", 1f, 0f)
+                val playerEventListener = object : EventListener {
+                    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                        if (playbackState == ExoPlayer.STATE_READY && !fader.isStarted) {
+                            with(fader) {
+                                // fade-out over 10s or the entire audio, whichever quicker
+                                duration = min(contentDuration, TimeUnit.SECONDS.toMillis(10))
+                                // trigger fade-out at 10s before audio-end or at audio-start, whichever later
+                                startDelay = max(0, contentDuration.minus(duration))
+                                start()
+                            }
+                        }
+                    }
+                }
+                fader.doOnEnd {
+                    audioComponent?.volume = 1f
+                    removeListener(playerEventListener)
+                }
+                addListener(playerEventListener)
                 playWhenReady = true
             }
     }
